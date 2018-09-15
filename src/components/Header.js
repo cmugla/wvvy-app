9134f9e4c4e22305c0be4fc2bcbbcfb5e9fea4f5
Celeste Glavin <cmugla@gmail.com>
2018-09-15 15:43:23 -0400

    try some extra logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,6 @@ const Config = {
   IS_IOS: !!navigator.userAgent.match(/iPhone|iPad|iPod/i),
 }
 
-const openFile = (e) => {
-  if(Config.IS_IOS) {
-    window.open(podcast);
-  }
-}
-
 const Header = (props) => {
   let extraProps = {}
   if(Config.IS_IOS) {
@@ -27,7 +21,7 @@ const Header = (props) => {
         <p>Welcome to WVVY. We are a fully volunteer-run radio station located on Martha's Vineyard, MA.</p>
         <p>Check out the <a href="#calendar">DJ schedule</a>, <a href="#listen">what's playing now</a>, and <a href="#contact">our support options</a> below. We are accepting requests and feedback all the time. Thank you for tuning in.</p>
       </div>
-      <a style={{ fontSize: '30px', color: 'dodgerblue' }} download href={podcast} onClick={openFile} {...extraProps}>Download Test</a>
+      <a style={{ fontSize: '30px', color: 'dodgerblue' }} download href={podcast} {...extraProps}>Download Test, Click</a>
     </header>
   )
 }
