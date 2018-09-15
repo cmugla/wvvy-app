fe4f9b95e9c0ff8443207b4a942f3de049e22b43
Celeste Glavin <cmugla@gmail.com>
2018-09-15 14:38:00 -0400

    update to https

--- src/components/NowPlaying.js
+++ src/components/NowPlaying.js
@@ -4,7 +4,7 @@ const NowPlaying = props => {
   return (
     <div className="nowplaying">
       <div className="embed-container">
-        <iframe title="wvvy now playing" src="http://wvvy.org/nowplaying.html" style={{ border: '0' }} />
+        <iframe title="wvvy now playing" src="https://wvvy.org/nowplaying.html" style={{ border: '0' }} />
       </div>
     </div>
   )
