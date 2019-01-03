57ec9a7ff47b9ab515f29e72139206f6ffed6a85
Celeste Glavin <celeste.glavin@wework.com>
2019-01-02 20:10:42 -0500

    add Al Kooper message

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,16 @@
-const weeksBetween = (d1, d2) => Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+export const weeksBetween = () => {
+  const now = new Date();
+  const then = new Date('September 23, 2018 11:00:00');
+  return Math.ceil((now - then) / (7 * 24 * 60 * 60 * 1000))
+};
 
 export const getEpNumber = () => {
-  const now = new Date()
-  const startDate = new Date('September 23, 2018 11:00:00');
-  return weeksBetween(startDate, now);
+  const diff = weeksBetween();
+
+  // Show is paused for two weeks!
+  if(diff === 15 || diff === 16 || diff === 17) {
+    return 15
+  }
+
+  return diff - 2;
 }
